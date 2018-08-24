a73d1b863855a02c1a9972558289c898545b45c1
gtsakiri <tsakiridis@serres.gr>
2018-08-24 12:02:28 +0300

    Diplomatiki TEI app, first commit

---
+++ Elections/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+
+from .models import Edres, Edreskoin, Eklogestbl, Eklper, Eklperkoin, Eklsimbkoin, Eklsimbper, Eklsind
+from .models import Eklsindkoin, Eklsindsimb, Kentra, Koinotites, Perifereies, Psifodeltia, Psifoi, Simbouloi, Sindiasmoi
+from .models import Sistima, Typeofkoinotita, EklSumpsifodeltiasindVw, EklSumpsifoiKenVw
+
+
+admin.site.register(Edres)
+admin.site.register(Edreskoin)
+admin.site.register(Eklogestbl)
+admin.site.register(Eklper)
+admin.site.register(Eklperkoin)
+admin.site.register(Eklsimbkoin)
+admin.site.register(Eklsimbper)
+admin.site.register(Eklsind)
+admin.site.register(Eklsindkoin)
+admin.site.register(Eklsindsimb)
+admin.site.register(Kentra)
+admin.site.register(Koinotites)
+admin.site.register(Perifereies)
+admin.site.register(Psifodeltia)
+admin.site.register(Psifoi)
+admin.site.register(Simbouloi)
+admin.site.register(Sindiasmoi)
+admin.site.register(Sistima)
+admin.site.register(Typeofkoinotita)
+
+
+
+#admin.site.register(EklSumpsifodeltiasindVw)
+#admin.site.register(EklSumpsifoiKenVw)
+
