54b7b6aa01b9ea4fba3ace26a9a3bc233fe9c8b2
gtsakiri <tsakiridis@serres.gr>
2018-08-26 22:27:56 +0300

    work 26/08/2018

--- Elections/forms.py
+++ Elections/forms.py
@@ -1,7 +1,18 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, forms
 from .models import Edres, Eklogestbl
 
 class EdresForm(ModelForm):
     class Meta:
         model=Edres
         fields = '__all__'
+
+    def clean(self):
+        cleaned_data = super(EdresForm, self).clean()
+        descr = cleaned_data.get('descr')
+        sinoloedrwn = cleaned_data.get('sinoloedrwn')
+        edresprwtou = cleaned_data.get('edresprwtou')
+        edresypoloipwn = cleaned_data.get('edresypoloipwn')
+        # if not name and not email and not message:
+        if sinoloedrwn != (edresprwtou + edresypoloipwn):
+            raise forms.ValidationError('Το σύνολο των εδρών πρέπει να ισούται με το άθροισμα των δύο άλλων σχετικών πεδίων!')
+
