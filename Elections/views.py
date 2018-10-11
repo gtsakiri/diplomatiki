451f26df20a749ca885e06236297174234cceae8
gtsakiri <tsakiridis@serres.gr>
2018-10-11 13:11:28 +0300

    work_10_10_2018

--- Elections/views.py
+++ Elections/views.py
@@ -474,7 +474,7 @@ def pososta_telika(request, eklid):
     # επιλογή όλων των εκλ. αναμετρήσεων με visible=1 και κάνω φθίνουσα ταξινόμηση  αν δεν δοθεί παράμετρος
     all_ekloges = Eklogestbl.objects.filter(visible=1).order_by('-eklid')
     #ανάκτηση εγγραφών επιλεγμένης εκλ. αναμέτρησης από το σχετικό database view
-    all_pososta = EklSumpsifodeltiasindVw.objects.filter(eklid=eklid).values_list('shortdescr', 'posostosindiasmou','posostokatametrimenwnkentrwn', 'photo').order_by('-posostosindiasmou')
+    all_pososta = EklSumpsifodeltiasindVw.objects.filter(eklid=eklid).order_by('-posostosindiasmou')
 
     context = {'all_pososta':all_pososta, 'all_ekloges':all_ekloges, 'selected_ekloges':selected_ekloges.eklid}
 
